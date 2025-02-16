26b31f5e5b0a01495b4070684524b21902c20a46
Ashish Varma <ashishvarma080213@gmail.com>
2025-02-16 22:22:06 +0530

    Adding Single Linear Regression

---
+++ Machine Learning with Python/Single Linear Regression/linearRegressionTemp.py
@@ -0,0 +1,21 @@
+# Creating Custom Generated Data
+
+import sklearn
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+# y = mx + c
+# F = 1.8*C + 32
+
+x = list(range(0, 50)) # Celsius Temperatiure
+y = [1.8*F + 32 + random.randint(-3, 3) for F in x] # Noisy Fahrenheit Temperature
+# y = [1.8*F + 32 + random.randint(-3, 3) for F in x] # Noisy Fahrenheit Temperature
+
+print(f'X: {x}')
+print(f'Y: {y}')
+
+plt.plot(x, y, '-*r')
+plt.show()
+
+
